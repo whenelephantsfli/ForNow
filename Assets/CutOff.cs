f65fc6452014897e1940c3126e4397409f119c6b
Allison Comrie <allisoncomrie12@gmail.com>
2016-09-22 09:30:49 -0700

    sound design almost complete...added door mechanics and played with FPScontroller

---
+++ Assets/CutOff.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutOff : MonoBehaviour {
+	bool disable = false; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit(Collider other){
+	
+
+		if (other.CompareTag("Player") && GetComponentInChildren<DoorOpen>().opened == true){
+
+			print ("Door: closeTriggered");
+			GetComponentInChildren<DoorOpen>().Close(); 
+			disable = true;
+		
+	
+		}
+
+
+		if (other.CompareTag("Player")){
+		gameObject.GetComponent<BoxCollider>().isTrigger = false;
+
+
+		}
+	}
+
+}
---
+++ Assets/DoorTrigger.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrigger : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	void OnTriggerEnter(Collider other){
+
+		if (other.CompareTag("Player")){
+			print ("Door: openTriggered");
+		GetComponentInChildren<DoorOpen>().Open(); 
+
+		}
+	} 
+
+	void OnTriggerExit(Collider other){
+
+		if (other.CompareTag("Player") && GetComponentInChildren<DoorOpen>().opened == true){
+
+			print ("Door: closeTriggered");
+			GetComponentInChildren<DoorOpen>().Close(); 
+
+		}
+
+		}
+
+}
